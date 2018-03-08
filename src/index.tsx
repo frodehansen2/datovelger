cf5006e2748f3cb49e00469f9cee9aeb7cfe91e6
Frode Hansen <frode.hansen@bekk.no>
2018-03-08 09:43:52 +0100

    rydd

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'moment/locale/nb';
 
+import 'moment/locale/nb';
 import 'react-dates/initialize';
 
-import './index.css';
-
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
-registerServiceWorker();
